0aebf388eec83c5ba100aa3a88da38a270be0a29
kiyomin0615 <moeday1010@gmail.com>
2024-09-09 20:53:17 +0900

    handle errors

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const csrf = require("csurf");
 const authRouter = require("./routes/auth-routes");
 const mongodb = require("./database/mongodb");
 const addCsrfToken = require("./middlewares/addCsrfToken")
+const handleErrors = require("./middlewares/handleErrors");
 
 const app = express();
 
@@ -19,6 +20,8 @@ app.use(csrf()); // csrf middleware denys POST requests which do not have csrf t
 app.use(addCsrfToken); // addCsrfToken middleware adds and gives csrf token to clients
 
 app.use(authRouter);
+
+app.use(handleErrors); // handleErrors middleware handles errors
 
 mongodb
   .connectToDatabase()
