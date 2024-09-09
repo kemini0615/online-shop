a6edfd9234c9c2edcf52369b42e27c06796c10bf
kiyomin0615 <moeday1010@gmail.com>
2024-09-09 15:47:24 +0900

    add base.css file for base styles

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views")); // __dirname is a path to this project directory
 
+app.use(express.static("public")); // static middleware handles requrests for static files such as css or js within 'public' directory
+
 app.use(authRouter);
 
 app.listen(3000);
