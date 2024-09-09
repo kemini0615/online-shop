71f51f3f4b8d49e40020778dc2593875e9be6836
kiyomin0615 <moeday1010@gmail.com>
2024-09-09 13:44:38 +0900

    add first ejs views

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
+const path = require("path");
+
 const express = require("express");
 const authRouter = require("./routes/auth-routes");
 
 const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views")); // __dirname is a path to this project directory
+
 app.use(authRouter);
 
 app.listen(3000);
