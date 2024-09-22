65fc02e6a15d429d9122992233faece67d37a862
kiyomin0615 <moeday1010@gmail.com>
2024-09-22 15:18:40 +0900

    add orders page

--- models/order.js
+++ models/order.js
@@ -1,3 +1,5 @@
+const mongodb = require("mongodb");
+
 const db = require("../database/mongodb");
 
 class Order {
@@ -13,9 +15,50 @@ class Order {
     this.id = orderId;
   }
 
+  static transformOrderDocuments(orderDocuments) {
+    return orderDocuments.map((orderDocument) => {
+      return new Order(
+        orderDocument.userInfo,
+        orderDocument.productInfo,
+        orderDocument.status,
+        orderDocument.date,
+        orderDocument._id
+      );
+    })
+  }
+
+  static async findAll() {
+    const allOrderDocuments = await db.getDatabase().collection("orders").find().sort({ _id: -1 }).toArray(); // descending order by id
+    return this.transformOrderDocuments(allOrderDocuments);
+  }
+
+  static async findAllForUser(userId) {
+    const uid = new mongodb.ObjectId(userId);
+
+    const orderDocuments = await db.getDatabase().collection("orders").find({ "userInfo._id": uid }).sort({ _id: -1 }).toArray();
+
+    return this.transformOrderDocuments(orderDocuments);
+  }
+
+  static async findById(orderId) {
+    const orderDocument = await db.getDatabase().collection("orders").findOne({ _id: new mongodb.ObjectId(orderId) });
+
+    const order = new Order(
+      orderDocument.userInfo,
+      orderDocument.productInfo,
+      orderDocument.status,
+      orderDocument.date,
+      orderDocument._id
+    );
+    
+    return order;
+  }
+
   save() {
     if (this.id) {
       // update old order
+      const orderId = new mongodb.ObjectId(this.id);
+      db.getDatabase().collection("orders").updateOne({ _id: orderId }, { $set: { status: this.status } });
     } else {
       // create new order
       const orderDocument = {
